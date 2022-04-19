103326161aef27653c0abf41c97b58072e6aabb5
Igor Shaymukhametov <shaym@yandex.ru>
2022-04-19 13:37:10 +0500

    feat: add link block

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const goButton = document.querySelector('.go')
 const field = document.querySelector('.field')
+const link = document.querySelector('.link')
+let number = ''
 
 goButton.addEventListener('click', () => {
-    const value = field.value
-    const matches = value.matchAll(/\d*/gm)
-    let number = ''
+    if (number) window.open(`https://api.whatsapp.com/send/?phone=${number}`)    
+})
+
+field.addEventListener('input', () => {
+    getNumber()
+    link.textContent = `https://api.whatsapp.com/send/?phone=${number}`
+    link.href = `https://api.whatsapp.com/send/?phone=${number}`
+})
+
+function getNumber() {
+    number = ''
+    const value = field.value;
+    const matches = value.matchAll(/\d*/gm);
     for (let match of matches) {
         if (match[0]) number += match[0]
-        console.log(match)
     }
-    
-    if (number) window.open(`https://wa.me/${number[0] == '8' ? '7' + number.slice(1) : number}`)
-})
+    return number[0] == '8' ? '7' + number.slice(1) : number    
+}
